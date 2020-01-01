7e719064150fa2eafff9f791dc22ff53d7d0bf96
Takayuki Miyoshi <takayukister@gmail.com>
2020-01-02 02:07:09 +0900

    Convert spaces to tabs

--- includes/js/src/block-editor.js
+++ includes/js/src/block-editor.js
@@ -7,143 +7,143 @@ const { select, dispatch } = wp.data;
 const { apiFetch } = wp;
 
 function LanguagePanel() {
-  const currentPost = Object.assign( {},
-    select( 'core/editor' ).getCurrentPost(),
-    bogo.currentPost
-  );
+	const currentPost = Object.assign( {},
+		select( 'core/editor' ).getCurrentPost(),
+		bogo.currentPost
+	);
 
-  const [ translations, setTranslations ]
-    = useState( currentPost.translations );
+	const [ translations, setTranslations ]
+		= useState( currentPost.translations );
 
-  const PostLanguage = () => {
-    return(
-      <PanelRow>
-        <span>Language</span>
-        <div>{ getLanguage( currentPost.locale ) }</div>
-      </PanelRow>
-    );
-  }
+	const PostLanguage = () => {
+		return(
+			<PanelRow>
+				<span>Language</span>
+				<div>{ getLanguage( currentPost.locale ) }</div>
+			</PanelRow>
+		);
+	}
 
-  const Translations = () => {
-    const listItems = [];
+	const Translations = () => {
+		const listItems = [];
 
-    Object.entries( translations ).forEach( ( [ key, value ] ) => {
-      if ( value.editLink && value.postTitle ) {
-        listItems.push(
-          <li key={ key }>
-            <a
-              href={ value.editLink }
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              { value.postTitle }
-            </a>
-            <span className="screen-reader-text">(opens in a new window)</span>
-            <br />
-            <em>{ getLanguage( key ) }</em>
-          </li>
-        );
-      }
-    } );
+		Object.entries( translations ).forEach( ( [ key, value ] ) => {
+			if ( value.editLink && value.postTitle ) {
+				listItems.push(
+					<li key={ key }>
+						<a
+							href={ value.editLink }
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{ value.postTitle }
+						</a>
+						<span className="screen-reader-text">(opens in a new window)</span>
+						<br />
+						<em>{ getLanguage( key ) }</em>
+					</li>
+				);
+			}
+		} );
 
-    const ListItems = ( props ) => {
-      if ( props.listItems.length ) {
-        return(
-          <ul>{ props.listItems }</ul>
-        );
-      } else {
-        return(
-          <em>None</em>
-        );
-      }
-    }
+		const ListItems = ( props ) => {
+			if ( props.listItems.length ) {
+				return(
+					<ul>{ props.listItems }</ul>
+				);
+			} else {
+				return(
+					<em>None</em>
+				);
+			}
+		}
 
-    return(
-      <PanelRow>
-        <span>Translations</span>
-        <ListItems listItems={ listItems } />
-      </PanelRow>
-    );
-  }
+		return(
+			<PanelRow>
+				<span>Translations</span>
+				<ListItems listItems={ listItems } />
+			</PanelRow>
+		);
+	}
 
-  const AddTranslation = withState( {
-    locale: "",
-  } )( ( { locale, setState } ) => {
-    const options = [
-      { label: "Add Translation", value: "" }
-    ];
+	const AddTranslation = withState( {
+		locale: "",
+	} )( ( { locale, setState } ) => {
+		const options = [
+			{ label: "Add Translation", value: "" }
+		];
 
-    Object.entries( translations ).forEach( ( [ key, value ] ) => {
-      if ( ! value.postId ) {
-        options.push( { label: getLanguage( key ), value: key } );
-      }
-    } );
+		Object.entries( translations ).forEach( ( [ key, value ] ) => {
+			if ( ! value.postId ) {
+				options.push( { label: getLanguage( key ), value: key } );
+			}
+		} );
 
-    const addTranslation = ( locale ) => {
-      setState( { locale } );
+		const addTranslation = ( locale ) => {
+			setState( { locale } );
 
-      apiFetch( {
-        path: '/bogo/v1/posts/' + currentPost.id +
-          '/translations/' + locale,
-        method: 'POST',
-      } ).then( ( response ) => {
-        const translationsAlt = Object.assign( {}, translations );
+			apiFetch( {
+				path: '/bogo/v1/posts/' + currentPost.id +
+					'/translations/' + locale,
+				method: 'POST',
+			} ).then( ( response ) => {
+				const translationsAlt = Object.assign( {}, translations );
 
-        translationsAlt[ locale ] = {
-          postId: response[ locale ].id,
-          postTitle: response[ locale ].title.rendered,
-          editLink: response[ locale ].edit_link,
-        };
+				translationsAlt[ locale ] = {
+					postId: response[ locale ].id,
+					postTitle: response[ locale ].title.rendered,
+					editLink: response[ locale ].edit_link,
+				};
 
-        setTranslations( translationsAlt );
+				setTranslations( translationsAlt );
 
-        dispatch('core/notices').createInfoNotice(
-          'Translation post created.',
-          {
-            isDismissible: true,
-            type: 'snackbar',
-            speak: true,
-          }
-        );
-      } );
-    }
+				dispatch('core/notices').createInfoNotice(
+					'Translation post created.',
+					{
+						isDismissible: true,
+						type: 'snackbar',
+						speak: true,
+					}
+				);
+			} );
+		}
 
-    if ( options.length <= 1 || 'auto-draft' == currentPost.status ) {
-      return( <></> );
-    }
+		if ( options.length <= 1 || 'auto-draft' == currentPost.status ) {
+			return( <></> );
+		}
 
-    return(
-      <PanelRow>
-        <span></span>
-        <SelectControl
-          value={ locale }
-          options={ options }
-          onChange={ ( locale ) => { addTranslation( locale ) } }
-        />
-      </PanelRow>
-    );
-  } );
+		return(
+			<PanelRow>
+				<span></span>
+				<SelectControl
+					value={ locale }
+					options={ options }
+					onChange={ ( locale ) => { addTranslation( locale ) } }
+				/>
+			</PanelRow>
+		);
+	} );
 
-  return(
-    <PluginDocumentSettingPanel
-      name="bogo-language-panel"
-      title="Language"
-      className="bogo-language-panel"
-    >
-      <PostLanguage />
-      <Translations />
-      <AddTranslation />
-    </PluginDocumentSettingPanel>
-  );
+	return(
+		<PluginDocumentSettingPanel
+			name="bogo-language-panel"
+			title="Language"
+			className="bogo-language-panel"
+		>
+			<PostLanguage />
+			<Translations />
+			<AddTranslation />
+		</PluginDocumentSettingPanel>
+	);
 }
 
 registerPlugin( 'bogo-language-panel', {
-  render: LanguagePanel,
-  icon: 'translation'
+	render: LanguagePanel,
+	icon: 'translation'
 } );
 
 const getLanguage = ( locale ) => {
-  return bogo.availableLanguages[locale]
-    ? bogo.availableLanguages[locale]
-    : locale;
+	return bogo.availableLanguages[locale]
+		? bogo.availableLanguages[locale]
+		: locale;
 }
