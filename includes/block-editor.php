eb3d6778eb315c7b4763efdecefd00a36854b802
Takayuki Miyoshi <takayukister@gmail.com>
2020-01-04 19:07:47 +0900

    Fix the JS version to the plugin

--- includes/block-editor.php
+++ includes/block-editor.php
@@ -7,6 +7,6 @@ function bogo_enqueue_block_editor_assets() {
 		'bogo-block-editor',
 		plugins_url( 'includes/js/block-editor.js', BOGO_PLUGIN_BASENAME ),
 		array( 'wp-plugins', 'wp-edit-post', 'wp-components', 'wp-element' ),
-		sprintf( '%s-%s', BOGO_VERSION, time() )
+		BOGO_VERSION
 	);
 }
