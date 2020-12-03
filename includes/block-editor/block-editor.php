1ced8ba20f856b43f990870c5e002c6c67baea53
Takayuki Miyoshi <takayukister@gmail.com>
2020-12-03 18:23:41 +0900

    Update blockeditor scripts

---
+++ includes/block-editor/block-editor.php
@@ -0,0 +1,48 @@
+<?php
+
+add_action(
+	'enqueue_block_editor_assets',
+	'bogo_init_block_editor_assets',
+	10, 0
+);
+
+function bogo_init_block_editor_assets() {
+	$assets = array();
+
+	$asset_file = path_join(
+		BOGO_PLUGIN_DIR,
+		'includes/block-editor/index.asset.php'
+	);
+
+	if ( file_exists( $asset_file ) ) {
+		$assets = include( $asset_file );
+	}
+
+	$assets = wp_parse_args( $assets, array(
+		'src' => plugins_url(
+			'includes/block-editor/index.js',
+			BOGO_PLUGIN_BASENAME
+		),
+		'dependencies' => array(
+			'wp-components',
+			'wp-data',
+			'wp-edit-post',
+			'wp-element',
+			'wp-plugins',
+			'wp-i18n',
+		),
+		'version' => BOGO_VERSION,
+	) );
+
+	wp_enqueue_script(
+		'bogo-block-editor',
+		$assets['src'],
+		$assets['dependencies'],
+		$assets['version']
+	);
+
+	wp_set_script_translations(
+		'bogo-block-editor',
+		'bogo'
+	);
+}
