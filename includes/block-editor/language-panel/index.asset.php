55179f17bdb046651ad20a468e7828c421d62b30
Takayuki Miyoshi <takayukister@gmail.com>
2023-02-10 17:58:53 +0900

    Create dummy asset file
    #232

---
+++ includes/block-editor/language-panel/index.asset.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'dependencies' => array(
+		'wp-api-fetch',
+		'wp-components',
+		'wp-data',
+		'wp-edit-post',
+		'wp-element',
+		'wp-i18n',
+		'wp-plugins',
+		'wp-url',
+	),
+	'version' => BOGO_VERSION,
+);
