82267e9d3a8d65157a7c996dca6c163f033aeeda
Takayuki Miyoshi <takayukister@gmail.com>
2023-03-28 17:36:07 +0900

    bogo_language_suggestion() rough sketch
    #240

--- includes/language-suggestion.php
+++ includes/language-suggestion.php
@@ -5,5 +5,23 @@ function bogo_language_suggestion( $args = '' ) {
 		'echo' => false,
 	) );
 
-  return 'test';
+	$locale_to_suggest = false;
+
+	foreach ( bogo_http_accept_languages() as $locale ) {
+		$locale_to_suggest = bogo_get_closest_locale( $locale );
+
+		if ( $locale_to_suggest ) {
+			break;
+		}
+	}
+
+	$output = $locale_to_suggest;
+
+	$output = apply_filters( 'bogo_language_suggestion', $output, $args );
+
+	if ( $args['echo'] ) {
+		echo $output;
+	} else {
+		return $output;
+	}
 }
