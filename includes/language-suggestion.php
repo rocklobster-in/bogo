47c259039314936ccc53089532ea5d282d5e8dd1
Takayuki Miyoshi <takayukister@gmail.com>
2023-03-30 17:03:26 +0900

    Use a link for suggestion
    #240

--- includes/language-suggestion.php
+++ includes/language-suggestion.php
@@ -15,14 +15,32 @@ function bogo_language_suggestion( $args = '' ) {
 		}
 	}
 
-	switch_to_locale( $locale_to_suggest );
+	$output = '';
 
-	$output = sprintf(
-		'This plugin is also available in %1$s.',
-		bogo_get_language( $locale_to_suggest )
-	);
+	if ( $locale_to_suggest ) {
+		$lang_name = bogo_get_language( $locale_to_suggest );
+		$lang_tag = bogo_language_tag( $locale_to_suggest );
 
-	restore_previous_locale();
+		switch_to_locale( $locale_to_suggest );
+
+		$link = sprintf(
+			'<a %1$s>%2$s</a>',
+			bogo_format_atts( array(
+				'rel' => 'alternate',
+				'hreflang' => $lang_tag,
+				'href' => bogo_url(),
+				'title' => $lang_name ? $lang_name : $lang_tag,
+			) ),
+			bogo_get_language( $locale_to_suggest )
+		);
+
+		$output = sprintf(
+			'This plugin is also available in %1$s.',
+			$link
+		);
+
+		restore_previous_locale();
+	}
 
 	$output = apply_filters( 'bogo_language_suggestion', $output, $args );
 
