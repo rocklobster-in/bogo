2974a8bb942c52d817f630671085de8588dab5d9
Takayuki Miyoshi <takayukister@gmail.com>
2025-08-28 17:08:22 +0900

    Remove unused JS code
    #333

--- admin/includes/js/admin.js
+++ admin/includes/js/admin.js
@@ -6,65 +6,6 @@
 		return;
 	}
 
-	bogo.langName = function( locale ) {
-		if ( bogo.availableLanguages[ locale ] ) {
-			return bogo.availableLanguages[ locale ].name;
-		} else {
-			return '';
-		}
-	};
-
-	$( function() {
-		$( '#bogo-add-translation' ).click( function() {
-			if ( ! bogo.currentPost.postId ) {
-				return;
-			}
-
-			var locale = $( '#bogo-translations-to-add' ).val();
-			var rest_url = bogo.apiSettings.getRoute(
-				'/posts/' + bogo.currentPost.postId + '/translations/' + locale );
-			$( '#bogo-add-translation' ).next( '.spinner' )
-				.css( 'visibility', 'visible' );
-
-			$.ajax( {
-				type: 'POST',
-				url: rest_url,
-				beforeSend: function( xhr ) {
-					xhr.setRequestHeader( 'X-WP-Nonce', bogo.apiSettings.nonce );
-				}
-			} ).done( function( response ) {
-				var post = response[ locale ];
-
-				if ( ! post ) {
-					return;
-				}
-
-				var $added = $( '<a></a>' ).attr( {
-					href: post.edit_link,
-					target: '_blank',
-					rel: 'noopener noreferrer'
-				} ).html( function() {
-					var output = post.title.rendered;
-					output += ' <span class="screen-reader-text">'
-						+ bogo.l10n.targetBlank + '</span>';
-					return output;
-				} );
-
-				$added = $( '<li></li>' ).append( $added ).append(
-					' [' + bogo.langName( locale ) + ']' );
-				$( '#bogo-translations' ).append( $added );
-
-				$( '#bogo-translations-to-add option[value="' + locale + '"]' ).detach();
-
-				if ( $( '#bogo-translations-to-add option' ).length < 1 ) {
-					$( '#bogo-add-translation-actions' ).detach();
-				}
-			} ).always( function() {
-				$( '#bogo-add-translation' ).next( '.spinner' ).css( 'visibility', 'hidden' );
-			} );
-		} );
-	} );
-
 	$( function() {
 		if ( 'bogo-texts' == bogo.pagenow ) {
 			$( window ).on( 'beforeunload', function( event ) {
@@ -91,15 +32,5 @@
 			} );
 		}
 	} );
-
-	bogo.apiSettings.getRoute = function( path ) {
-		var url = bogo.apiSettings.root;
-
-		url = url.replace(
-			bogo.apiSettings.namespace,
-			bogo.apiSettings.namespace + path );
-
-		return url;
-	};
 
 } )( jQuery );
