97885591972fcd3aad8f345bd2eb7367598ab113
Takayuki Miyoshi <takayukister@gmail.com>
2025-09-14 16:24:48 +0900

    Apply wp_kses()
    #325

--- admin/includes/widgets.php
+++ admin/includes/widgets.php
@@ -20,14 +20,37 @@ function bogo_in_widget_form( $widget, $return, $instance ) {
 ?>
 <fieldset class="bogo-locale-options">
 <legend><?php echo esc_html( __( 'Displayed on pages in', 'bogo' ) ); ?></legend>
-<?php foreach ( $available_languages as $locale => $language ) :
-	$checked = in_array( $locale, $selected_languages );
-	$id_attr = $widget->get_field_id( 'bogo_locales' ) . '-' . $locale;
+<?php
+
+	foreach ( $available_languages as $locale => $language ) {
+		$checked = in_array( $locale, $selected_languages );
+
+		$id_attr = sprintf(
+			'%s-%s',
+			$widget->get_field_id( 'bogo_locales' ),
+			$locale
+		);
+
+		$checkbox = sprintf(
+			'<label %1$s><input %2$s />%3$s</label>',
+			bogo_format_atts( array(
+				'class' => 'bogo-locale-option' . ( $checked ? ' checked' : '' ),
+				'for' => $id_attr,
+			) ),
+			bogo_format_atts( array(
+				'type' => 'checkbox',
+				'id' => $id_attr,
+				'name' => sprintf( '%s[]', $widget->get_field_name( 'bogo_locales' ) ),
+				'value' => $locale,
+				'checked' => $checked,
+			) ),
+			$language
+		);
+
+		echo wp_kses( $checkbox, 'bogo_form_inside' ) . "\n";
+	}
+
 ?>
-<label class="bogo-locale-option<?php echo $checked ? ' checked' : ''; ?>" for="<?php echo $id_attr; ?>">
-<input type="checkbox" id="<?php echo $id_attr; ?>" name="<?php echo $widget->get_field_name( 'bogo_locales' ); ?>[]" value="<?php echo esc_attr( $locale ); ?>"<?php echo $checked ? ' checked="checked"' : ''; ?> /><?php echo esc_html( $language ); ?>
-</label>
-<?php endforeach; ?>
 </fieldset>
 <?php
 }
