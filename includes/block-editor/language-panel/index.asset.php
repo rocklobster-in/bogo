fb05d1c7b1ad8e31e571b3b5bfa0f24e5bf033ec
davidperezgar <david@closemarketing.es>
2025-10-21 18:13:14 +0200

    reverted change

--- includes/block-editor/language-panel/index.asset.php
+++ includes/block-editor/language-panel/index.asset.php
@@ -1,1 +1,16 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '603a79a9263ad39912ab');
+<?php
+
+return array(
+	'dependencies' => array(
+		'react-jsx-runtime',
+		'wp-api-fetch',
+		'wp-components',
+		'wp-data',
+		'wp-edit-post',
+		'wp-element',
+		'wp-i18n',
+		'wp-plugins',
+		'wp-url',
+	),
+	'version' => BOGO_VERSION,
+);
