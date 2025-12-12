efe5657777843a88159e7f634c2914c6165cc013
Lovro Hrust <hrustlovro@gmail.com>
2025-12-13 00:35:03 +0100

    improved language switcher block render

--- includes/block-editor/language-switcher/index.asset.php
+++ includes/block-editor/language-switcher/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '6808808362078a93f661');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '2ca9356f0b5c9021ffee');
